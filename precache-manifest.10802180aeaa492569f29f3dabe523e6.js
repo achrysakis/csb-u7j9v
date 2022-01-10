a8177f3a2fb1a16115beb00d990a503637462090
Code Sandbox <hello@codesandbox.io>
2022-01-10 13:22:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.10802180aeaa492569f29f3dabe523e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740fff90059c794541a71f1f15e83a8f",
+    "url": "https://achrysakis.github.io/csb-u7j9v/index.html"
+  },
+  {
+    "revision": "aadea04ca721e96c24b8",
+    "url": "https://achrysakis.github.io/csb-u7j9v/static/js/2.0a6b8640.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://achrysakis.github.io/csb-u7j9v/static/js/2.0a6b8640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ca2ab507984f8185b4",
+    "url": "https://achrysakis.github.io/csb-u7j9v/static/js/main.5d56ff63.chunk.js"
+  },
+  {
+    "revision": "9f319fd27bb6139f5eea",
+    "url": "https://achrysakis.github.io/csb-u7j9v/static/js/runtime-main.7f008dc4.js"
+  }
+]);
